a916462cadf2e463b66299b10efef84337c05a89
Trevor Wong <trevor8568@gmail.com>
2020-05-19 03:19:52 +0000

    Finished item importer and pushed items to db

--- item_data_import/ItemParser.py
+++ item_data_import/ItemParser.py
@@ -1,4 +1,17 @@
-import os 
+import json, os, mysql.connector
+
+secret_json = json.loads(open('../secret.json').read())
+db_user = secret_json['db_user']
+db_pass = secret_json['db_pass']
+
+db = mysql.connector.connect(
+    host='localhost',
+    user=db_user,
+    passwd=db_pass,
+    database='shopstock'
+)
+
+cursor = db.cursor()
 
 itemsDictionary = {}
 with open('all-items.txt', 'r') as file:
@@ -14,3 +27,22 @@ with open('all-items.txt', 'r') as file:
         else:
             isCategory = True
 print(itemsDictionary)
+
+for category in itemsDictionary.keys():
+    try:
+        cursor.execute("INSERT INTO item_category_lookup VALUES (NULL,'" + category + "')")
+    except mysql.connector.errors.IntegrityError:
+        print('Category ' + category + ' already exists!')
+        pass
+    
+    cursor.execute("SELECT id FROM item_category_lookup WHERE name = '" + category + "'")
+    cat_id = cursor.fetchone()[0]
+    print('Inserting category %s with id %d'%(category, cat_id))
+    for item in itemsDictionary[category]:
+        try:
+            cursor.execute("INSERT INTO item_lookup VALUES (NULL,'" + item + "', " + str(cat_id) + ")")
+        except mysql.connector.errors.IntegrityError:
+            print('Item ' + item + ' already exists!')
+            pass
+
+db.commit()
